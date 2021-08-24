402e6b738048b42fb10e107471a12d1d1bf317c8
Xiepf <xiepengfei1@xiaomi.com>
2021-08-24 11:58:32 +0800

    solution 18

---
+++ src/LeetCodeSolution/Array.java
@@ -0,0 +1,198 @@
+package LeetCodeSolution;
+
+import com.sun.javafx.image.IntPixelGetter;
+
+import javax.print.attribute.standard.PresentationDirection;
+import java.util.*;
+
+public class Array {
+
+
+    class Solution1 {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+            for (int i = 0; i < nums.length; i++) {
+                if (map.containsKey(target - nums[i])) {
+                    return new int[]{i, map.get(target - nums[i])};
+                }
+                map.put(nums[i], i);
+            }
+            return null;
+        }
+    }
+
+    class Solution11 {
+        public int maxArea(int[] height) {
+            int start = 0;
+            int end = height.length - 1;
+            int maxSize = 0;
+            while (start < end) {
+                int size = (end - start) * Math.min(height[start], height[end]);
+                if (size > maxSize) {
+                    maxSize = size;
+                }
+                if (height[start] >= height[end]) {
+                    end--;
+                } else {
+                    start++;
+                }
+            }
+            return maxSize;
+        }
+    }
+
+    class Solution15 {
+        public List<List<Integer>> threeSum(int[] nums) {
+            List<List<Integer>> result = new ArrayList<>();
+            Arrays.sort(nums);
+            int len = nums.length;
+            for (int i = 0; i < len - 2; i++) {
+                if (nums[i] > 0) {
+                    return result;
+                }
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                int curNum = nums[i];
+                int left = i + 1;
+                int right = len - 1;
+                while (left < right) {
+                    if (curNum + nums[left] + nums[right] == 0) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(curNum);
+                        list.add(nums[left]);
+                        list.add(nums[right]);
+                        result.add(list);
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    } else if (curNum + nums[left] + nums[right] > 0) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+    class Solution16 {
+        public int threeSumClosest(int[] nums, int target) {
+            Arrays.sort(nums);
+            int len = nums.length;
+            //不能赋int MaxValue 会产生越界 默认值最好设为某个可能解
+            int min = nums[0] + nums[1] + nums[len-1];
+            for (int i = 0; i < len - 2; i++) {
+                int curNum = nums[i];
+                int left = i + 1;
+                int right = len - 1;
+                while (left < right) {
+                    int total = curNum + nums[left] + nums[right];
+                    if (Math.abs(target - total) < Math.abs(target - min)) {
+                        min = total;
+                    }
+                    if (total > target) {
+                        right--;
+                    } else if(total < target){
+                        left++;
+                    }else{
+                        return min;
+                    }
+                }
+            }
+            return min;
+        }
+    }
+
+     class Solution18 {
+        public List<List<Integer>> fourSum(int[] nums, int target) {
+            List<List<Integer>> result = new ArrayList<>();
+            Arrays.sort(nums);
+            int len = nums.length;
+            for(int i = 0; i < len - 3; i++){
+                if(i != 0 && nums[i-1] == nums[i]){
+                    continue;
+                }
+                if(nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target){
+                    break;
+                }
+                if(nums[i] + nums[len-1] + nums[len-2] + nums[len-3] < target){
+                    continue;
+                }
+                for(int j = i + 1; j < len - 2; j++){
+                    if(j > i + 1 && nums[j-1] == nums[j]){
+                        continue;
+                    }
+                    if(nums[i] + nums[j] + nums[j+1] + nums[j+2] > target){
+                        break;
+                    }
+                    if(nums[i] + nums[j] + nums[len-1] + nums[len-2] < target){
+                        continue;
+                    }
+                    int cur = nums[i] + nums[j];
+                    int left = j + 1;
+                    int right = len - 1;
+                    while(left < right){
+                        if(cur + nums[left] + nums[right] == target){
+                            List<Integer> list = Arrays.asList(nums[i], nums[j], nums[left], nums[right]);
+                            result.add(list);
+                            while(left < right && nums[left+1] == nums[left]) left++;
+                            while(left < right && nums[right-1] == nums[right]) right--;
+                            left++;
+                            right--;
+                        }
+                        else if(cur + nums[left] + nums[right] < target) left++;
+                        else right--;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+    class Solution18dfs {
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        int cur = 0;
+        public List<List<Integer>> fourSum(int[] nums, int target) {
+            Arrays.sort(nums);
+            dfs(nums, target,0);
+            return ans;
+        }
+        public void dfs(int[] nums, int target, int sub){
+            if(list.size() == 4){
+                if(cur == target){
+                    ans.add(new ArrayList<>(list));
+                }
+                return;
+            }
+            for(int i = sub; i < nums.length; i++){
+                if(4 - list.size() > nums.length - i) return;
+                if(i > sub && nums[i] == nums[i-1]) continue;
+                if(i < nums.length - 1 && cur + nums[i] + (3 - list.size()) * nums[i + 1] > target) return;
+                if(i < nums.length - 1 && cur + nums[i] + (3 - list.size()) * nums[nums.length - 1] < target) continue;
+                cur += nums[i];
+                list.add(nums[i]);
+                dfs(nums, target, i+1);
+                cur -= nums[i];
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
